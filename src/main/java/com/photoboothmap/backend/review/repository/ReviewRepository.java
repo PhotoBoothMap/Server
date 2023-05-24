0196a46581d6f3444520cbd91e4eb54e64ecd710
Dasom Ahn <lizzy0521@naver.com>
2023-05-25 00:11:09 +0900

    ⛑️[FIX] : IllegalStateException

--- src/main/java/com/photoboothmap/backend/review/repository/ReviewRepository.java
+++ src/main/java/com/photoboothmap/backend/review/repository/ReviewRepository.java
@@ -3,6 +3,7 @@ package com.photoboothmap.backend.review.repository;
 import com.photoboothmap.backend.review.entity.ReviewEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -11,5 +12,5 @@ public interface ReviewRepository extends JpaRepository<ReviewEntity, Long> {
 
     @Query(value = "select avg(star_rate) from review where photo_booth = :boothIdx"
             , nativeQuery = true)
-    Float averageStarRateByBoothIdx(Long boothIdx);
+    Float averageStarRateByBoothIdx(@Param("boothIdx") Long boothIdx);
 }
