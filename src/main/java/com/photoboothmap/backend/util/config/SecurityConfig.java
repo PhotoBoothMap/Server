e56538e4009826ede0f96cb3408cb046ade063a2
‍김용빈 <yongbin97@yonsei.ac.kr>
2023-06-14 00:00:54 +0900

    [FIX]: cors 2

--- src/main/java/com/photoboothmap/backend/util/config/SecurityConfig.java
+++ src/main/java/com/photoboothmap/backend/util/config/SecurityConfig.java
@@ -24,6 +24,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 @Slf4j
@@ -96,9 +97,11 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("*"));
-        configuration.setAllowedMethods(Arrays.asList("HEAD", "GET", "POST", "PUT"));
-        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
+        configuration.setAllowedOrigins(List.of("*"));
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
+        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setExposedHeaders(List.of("*"));
+
         configuration.setAllowCredentials(true);
 //        configuration.addAllowedOrigin("*");
 //        configuration.addAllowedHeader("*");
