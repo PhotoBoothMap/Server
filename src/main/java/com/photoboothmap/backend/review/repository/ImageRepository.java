8fec35788617188621ebabd366dc2e2abbf8eceb
Dasom Ahn <lizzy0521@naver.com>
2023-06-29 19:46:16 +0900

    ⛑️[FIX] : IllegalStateException

--- src/main/java/com/photoboothmap/backend/review/repository/ImageRepository.java
+++ src/main/java/com/photoboothmap/backend/review/repository/ImageRepository.java
@@ -4,6 +4,7 @@ import com.photoboothmap.backend.review.entity.ImageEntity;
 import com.photoboothmap.backend.review.entity.ReviewEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,5 +12,5 @@ import java.util.List;
 @Repository
 public interface ImageRepository extends JpaRepository<ImageEntity, Long> {
     @Query("SELECT img FROM ImageEntity img JOIN FETCH img.review WHERE img.review = :review")
-    List<ImageEntity> findByReviewWithJoin(ReviewEntity review);
+    List<ImageEntity> findByReviewWithJoin(@Param("review") ReviewEntity review);
 }
